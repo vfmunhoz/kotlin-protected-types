2c8d8a4f21b05270fdc305cfc3fa3b04c9626226
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2021-06-26 02:32:50 -0300

    feat(protected-long): add more use cases for long operators and unit tests

--- src/main/kotlin/com/vfmunhoz/protectedtypes/types/ProtectedLong.kt
+++ src/main/kotlin/com/vfmunhoz/protectedtypes/types/ProtectedLong.kt
@@ -17,6 +17,13 @@ value class ProtectedLong(val value: Long) : Comparable<ProtectedLong> {
     operator fun plus(other: Float): ProtectedFloat = ProtectedFloat(value + other)
     operator fun plus(other: Double): ProtectedDouble = ProtectedDouble(value + other)
 
+    operator fun plus(other: ProtectedByte): ProtectedLong = this.plus(other.value)
+    operator fun plus(other: ProtectedShort): ProtectedLong = this.plus(other.value)
+    operator fun plus(other: ProtectedInt): ProtectedLong = this.plus(other.value)
+    operator fun plus(other: ProtectedLong): ProtectedLong = this.plus(other.value)
+    operator fun plus(other: ProtectedFloat): ProtectedFloat = this.plus(other.value)
+    operator fun plus(other: ProtectedDouble): ProtectedDouble = this.plus(other.value)
+
     // Minus
     operator fun minus(other: Byte): ProtectedLong = ProtectedLong(value - other)
     operator fun minus(other: Short): ProtectedLong = ProtectedLong(value - other)
@@ -24,6 +31,13 @@ value class ProtectedLong(val value: Long) : Comparable<ProtectedLong> {
     operator fun minus(other: Long): ProtectedLong = ProtectedLong(value - other)
     operator fun minus(other: Float): ProtectedFloat = ProtectedFloat(value - other)
     operator fun minus(other: Double): ProtectedDouble = ProtectedDouble(value - other)
+
+    operator fun minus(other: ProtectedByte): ProtectedLong = this.minus(other.value)
+    operator fun minus(other: ProtectedShort): ProtectedLong = this.minus(other.value)
+    operator fun minus(other: ProtectedInt): ProtectedLong = this.minus(other.value)
+    operator fun minus(other: ProtectedLong): ProtectedLong = this.minus(other.value)
+    operator fun minus(other: ProtectedFloat): ProtectedFloat = this.minus(other.value)
+    operator fun minus(other: ProtectedDouble): ProtectedDouble = this.minus(other.value)
 
     // Times
     operator fun times(other: Byte): ProtectedLong = ProtectedLong(value * other)
@@ -33,6 +47,13 @@ value class ProtectedLong(val value: Long) : Comparable<ProtectedLong> {
     operator fun times(other: Float): ProtectedFloat = ProtectedFloat(value * other)
     operator fun times(other: Double): ProtectedDouble = ProtectedDouble(value * other)
 
+    operator fun times(other: ProtectedByte): ProtectedLong = this.times(other.value)
+    operator fun times(other: ProtectedShort): ProtectedLong = this.times(other.value)
+    operator fun times(other: ProtectedInt): ProtectedLong = this.times(other.value)
+    operator fun times(other: ProtectedLong): ProtectedLong = this.times(other.value)
+    operator fun times(other: ProtectedFloat): ProtectedFloat = this.times(other.value)
+    operator fun times(other: ProtectedDouble): ProtectedDouble = this.times(other.value)
+
     // Div
     operator fun div(other: Byte): ProtectedLong = ProtectedLong(value / other)
     operator fun div(other: Short): ProtectedLong = ProtectedLong(value / other)
@@ -40,6 +61,13 @@ value class ProtectedLong(val value: Long) : Comparable<ProtectedLong> {
     operator fun div(other: Long): ProtectedLong = ProtectedLong(value / other)
     operator fun div(other: Float): ProtectedFloat = ProtectedFloat(value / other)
     operator fun div(other: Double): ProtectedDouble = ProtectedDouble(value / other)
+
+    operator fun div(other: ProtectedByte): ProtectedLong = this.div(other.value)
+    operator fun div(other: ProtectedShort): ProtectedLong = this.div(other.value)
+    operator fun div(other: ProtectedInt): ProtectedLong = this.div(other.value)
+    operator fun div(other: ProtectedLong): ProtectedLong = this.div(other.value)
+    operator fun div(other: ProtectedFloat): ProtectedFloat = this.div(other.value)
+    operator fun div(other: ProtectedDouble): ProtectedDouble = this.div(other.value)
 
     // Rem
     operator fun rem(other: Byte): ProtectedLong = ProtectedLong(value % other)
@@ -49,8 +77,55 @@ value class ProtectedLong(val value: Long) : Comparable<ProtectedLong> {
     operator fun rem(other: Float): ProtectedFloat = ProtectedFloat(value % other)
     operator fun rem(other: Double): ProtectedDouble = ProtectedDouble(value % other)
 
+    operator fun rem(other: ProtectedByte): ProtectedLong = this.rem(other.value)
+    operator fun rem(other: ProtectedShort): ProtectedLong = this.rem(other.value)
+    operator fun rem(other: ProtectedInt): ProtectedLong = this.rem(other.value)
+    operator fun rem(other: ProtectedLong): ProtectedLong = this.rem(other.value)
+    operator fun rem(other: ProtectedFloat): ProtectedFloat = this.rem(other.value)
+    operator fun rem(other: ProtectedDouble): ProtectedDouble = this.rem(other.value)
+
     operator fun compareTo(other: Long): Int = value.compareTo(other)
     override operator fun compareTo(other: ProtectedLong): Int = compareTo(other.value)
 
     override fun toString(): String = "###$value###"
 }
+
+// Primitive + protected
+operator fun Long.plus(protectedValue: ProtectedByte): Long = this + protectedValue.value
+operator fun Long.plus(protectedValue: ProtectedShort): Long = this + protectedValue.value
+operator fun Long.plus(protectedValue: ProtectedInt): Long = this + protectedValue.value
+operator fun Long.plus(protectedValue: ProtectedLong): Long = this + protectedValue.value
+operator fun Long.plus(protectedValue: ProtectedFloat): Float = this + protectedValue.value
+operator fun Long.plus(protectedValue: ProtectedDouble): Double = this + protectedValue.value
+
+// Primitive - protected
+operator fun Long.minus(protectedValue: ProtectedByte): Long = this - protectedValue.value
+operator fun Long.minus(protectedValue: ProtectedShort): Long = this - protectedValue.value
+operator fun Long.minus(protectedValue: ProtectedInt): Long = this - protectedValue.value
+operator fun Long.minus(protectedValue: ProtectedLong): Long = this - protectedValue.value
+operator fun Long.minus(protectedValue: ProtectedFloat): Float = this - protectedValue.value
+operator fun Long.minus(protectedValue: ProtectedDouble): Double = this - protectedValue.value
+
+// Primitive * protected
+operator fun Long.times(protectedValue: ProtectedByte): Long = this * protectedValue.value
+operator fun Long.times(protectedValue: ProtectedShort): Long = this * protectedValue.value
+operator fun Long.times(protectedValue: ProtectedInt): Long = this * protectedValue.value
+operator fun Long.times(protectedValue: ProtectedLong): Long = this * protectedValue.value
+operator fun Long.times(protectedValue: ProtectedFloat): Float = this * protectedValue.value
+operator fun Long.times(protectedValue: ProtectedDouble): Double = this * protectedValue.value
+
+// Primitive / protected
+operator fun Long.div(protectedValue: ProtectedByte): Long = this / protectedValue.value
+operator fun Long.div(protectedValue: ProtectedShort): Long = this / protectedValue.value
+operator fun Long.div(protectedValue: ProtectedInt): Long = this / protectedValue.value
+operator fun Long.div(protectedValue: ProtectedLong): Long = this / protectedValue.value
+operator fun Long.div(protectedValue: ProtectedFloat): Float = this / protectedValue.value
+operator fun Long.div(protectedValue: ProtectedDouble): Double = this / protectedValue.value
+
+// Primitive % protected
+operator fun Long.rem(protectedValue: ProtectedByte): Long = this % protectedValue.value
+operator fun Long.rem(protectedValue: ProtectedShort): Long = this % protectedValue.value
+operator fun Long.rem(protectedValue: ProtectedInt): Long = this % protectedValue.value
+operator fun Long.rem(protectedValue: ProtectedLong): Long = this % protectedValue.value
+operator fun Long.rem(protectedValue: ProtectedFloat): Float = this % protectedValue.value
+operator fun Long.rem(protectedValue: ProtectedDouble): Double = this % protectedValue.value
---
+++ src/test/kotlin/com/vfmunhoz/protectedtypes/types/ProtectedLongTest.kt
@@ -0,0 +1,251 @@
+package com.vfmunhoz.protectedtypes.types
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class ProtectedLongTest {
+
+    private val primitiveByte: Byte = 10
+    private val primitiveShort: Short = 12
+    private val primitiveInt: Int = 14
+    private val primitiveLong: Long = 16
+    private val primitiveFloat: Float = 18.2f
+    private val primitiveDouble: Double = 20.8
+
+    private val protectedByte = ProtectedByte(primitiveByte)
+    private val protectedShort = ProtectedShort(primitiveShort)
+    private val protectedInt = ProtectedInt(primitiveInt)
+    private val protectedLong = ProtectedLong(primitiveLong)
+    private val protectedFloat = ProtectedFloat(primitiveFloat)
+    private val protectedDouble = ProtectedDouble(primitiveDouble)
+
+    private val maxProtectedLong = ProtectedLong(Long.MAX_VALUE)
+    private val minProtectedLong = ProtectedLong(Long.MIN_VALUE)
+
+    // Unary operators
+    @Test
+    fun `test the unary plus operator compared to Long type`() {
+        assertEquals(+primitiveLong, (+protectedLong).value)
+        assertEquals(primitiveLong.unaryPlus(), protectedLong.unaryPlus().value)
+
+        assertEquals(+Long.MAX_VALUE, (+maxProtectedLong).value)
+        assertEquals(+Long.MIN_VALUE, (+minProtectedLong).value)
+
+        assertEquals(Long.MAX_VALUE.unaryPlus(), (maxProtectedLong.unaryPlus()).value)
+        assertEquals(Long.MIN_VALUE.unaryPlus(), (minProtectedLong.unaryPlus()).value)
+    }
+
+    @Test
+    fun `test the unary minus operator compared to Long type`() {
+        assertEquals(-primitiveLong, (-protectedLong).value)
+        assertEquals(primitiveLong.unaryMinus(), protectedLong.unaryMinus().value)
+
+        assertEquals(-Long.MAX_VALUE, (-maxProtectedLong).value)
+        assertEquals(-Long.MIN_VALUE, (-minProtectedLong).value)
+
+        assertEquals(Long.MAX_VALUE.unaryMinus(), (maxProtectedLong.unaryMinus()).value)
+        assertEquals(Long.MIN_VALUE.unaryMinus(), (minProtectedLong.unaryMinus()).value)
+    }
+
+    // Increment and decrement operators
+    @Test
+    fun `test the increment operator compared to Long type`() {
+        var mutablePrimitive = primitiveFloat
+        var mutableProtected = protectedFloat
+
+        ++mutablePrimitive
+        ++mutableProtected
+
+        assertEquals(mutablePrimitive, mutableProtected.value)
+
+        mutablePrimitive++
+        mutableProtected++
+
+        assertEquals(mutablePrimitive, mutableProtected.value)
+
+        assertEquals(mutablePrimitive.inc(), mutableProtected.inc().value)
+
+        var mutableMaxPrimitive = Long.MAX_VALUE
+        var mutableMaxProtected = maxProtectedLong
+
+        assertEquals(++mutableMaxPrimitive, (++mutableMaxProtected).value)
+    }
+
+    @Test
+    fun `test the decrement operator compared to Long type`() {
+        var mutablePrimitive = primitiveFloat
+        var mutableProtected = protectedFloat
+
+        --mutablePrimitive
+        --mutableProtected
+
+        assertEquals(mutablePrimitive, mutableProtected.value)
+
+        mutablePrimitive--
+        mutableProtected--
+
+        assertEquals(mutablePrimitive, mutableProtected.value)
+
+        assertEquals(mutablePrimitive.dec(), mutableProtected.dec().value)
+
+        var mutableMaxPrimitiveDouble = Long.MIN_VALUE
+        var mutableMaxProtectedDouble = minProtectedLong
+
+        assertEquals(--mutableMaxPrimitiveDouble, (--mutableMaxProtectedDouble).value)
+    }
+
+    // Plus operator
+    @Test
+    fun `test the sum operator for ProtectedLong + primitive value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong + primitiveByte, (protectedLong + primitiveByte).value, "protectedLong.sum(primitiveByte)")
+        assertEquals(primitiveLong + primitiveShort, (protectedLong + primitiveShort).value, "protectedLong.sum(primitiveShort)")
+        assertEquals(primitiveLong + primitiveInt, (protectedLong + primitiveInt).value, "protectedLong.sum(primitiveInt)")
+        assertEquals(primitiveLong + primitiveLong, (protectedLong + primitiveLong).value, "protectedLong.sum(primitiveLong)")
+        assertEquals(primitiveLong + primitiveFloat, (protectedLong + primitiveFloat).value, "protectedLong.sum(primitiveFloat)")
+        assertEquals(primitiveLong + primitiveDouble, (protectedLong + primitiveDouble).value, "protectedLong.sum(primitiveDouble)")
+    }
+
+    @Test
+    fun `test the sum operator for ProtectedLong + protected value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong + primitiveByte, (protectedLong + protectedByte).value, "protectedLong.sum(protectedByte)")
+        assertEquals(primitiveLong + primitiveShort, (protectedLong + protectedShort).value, "protectedLong.sum(protectedShort)")
+        assertEquals(primitiveLong + primitiveInt, (protectedLong + protectedInt).value, "protectedLong.sum(protectedInt)")
+        assertEquals(primitiveLong + primitiveLong, (protectedLong + protectedLong).value, "protectedLong.sum(protectedLong)")
+        assertEquals(primitiveLong + primitiveFloat, (protectedLong + protectedFloat).value, "protectedLong.sum(protectedFloat)")
+        assertEquals(primitiveLong + primitiveDouble, (protectedLong + protectedDouble).value, "protectedLong.sum(protectedDouble)")
+    }
+
+    @Test
+    fun `test the sum operator for primitive value + protected value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong + primitiveByte, primitiveLong + protectedByte, "primitiveLong.sum(protectedByte)")
+        assertEquals(primitiveLong + primitiveShort, primitiveLong + protectedShort, "primitiveLong.sum(protectedShort)")
+        assertEquals(primitiveLong + primitiveInt, primitiveLong + protectedInt, "primitiveLong.sum(protectedInt)")
+        assertEquals(primitiveLong + primitiveLong, primitiveLong + protectedLong, "primitiveLong.sum(protectedLong)")
+        assertEquals(primitiveLong + primitiveFloat, primitiveLong + protectedFloat, "primitiveLong.sum(protectedFloat)")
+        assertEquals(primitiveLong + primitiveDouble, primitiveLong + protectedDouble, "primitiveLong.sum(protectedDouble)")
+    }
+
+    // Minus operator
+    @Test
+    fun `test the minus operator for ProtectedLong - primitive value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong - primitiveByte, (protectedLong - primitiveByte).value, "protectedLong.minus(primitiveByte)")
+        assertEquals(primitiveLong - primitiveShort, (protectedLong - primitiveShort).value, "protectedLong.minus(primitiveShort)")
+        assertEquals(primitiveLong - primitiveInt, (protectedLong - primitiveInt).value, "protectedLong.minus(primitiveInt)")
+        assertEquals(primitiveLong - primitiveLong, (protectedLong - primitiveLong).value, "protectedLong.minus(primitiveLong)")
+        assertEquals(primitiveLong - primitiveFloat, (protectedLong - primitiveFloat).value, "protectedLong.minus(primitiveFloat)")
+        assertEquals(primitiveLong - primitiveDouble, (protectedLong - primitiveDouble).value, "protectedLong.minus(primitiveDouble)")
+    }
+
+    @Test
+    fun `test the minus operator for ProtectedLong - protected value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong - primitiveByte, (protectedLong - protectedByte).value, "protectedLong.minus(protectedByte)")
+        assertEquals(primitiveLong - primitiveShort, (protectedLong - protectedShort).value, "protectedLong.minus(protectedShort)")
+        assertEquals(primitiveLong - primitiveInt, (protectedLong - protectedInt).value, "protectedLong.minus(protectedInt)")
+        assertEquals(primitiveLong - primitiveLong, (protectedLong - protectedLong).value, "protectedLong.minus(protectedLong)")
+        assertEquals(primitiveLong - primitiveFloat, (protectedLong - protectedFloat).value, "protectedLong.minus(protectedFloat)")
+        assertEquals(primitiveLong - primitiveDouble, (protectedLong - protectedDouble).value, "protectedLong.minus(protectedDouble)")
+    }
+
+    @Test
+    fun `test the minus operator for primitive value - protected value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong - primitiveByte, primitiveLong - protectedByte, "primitiveLong.minus(protectedByte)")
+        assertEquals(primitiveLong - primitiveShort, primitiveLong - protectedShort, "primitiveLong.minus(protectedShort)")
+        assertEquals(primitiveLong - primitiveInt, primitiveLong - protectedInt, "primitiveLong.minus(protectedInt)")
+        assertEquals(primitiveLong - primitiveLong, primitiveLong - protectedLong, "primitiveLong.minus(protectedLong)")
+        assertEquals(primitiveLong - primitiveFloat, primitiveLong - protectedFloat, "primitiveLong.minus(protectedFloat)")
+        assertEquals(primitiveLong - primitiveDouble, primitiveLong - protectedDouble, "primitiveLong.minus(protectedDouble)")
+    }
+
+    // Times operator
+    @Test
+    fun `test the times operator for ProtectedLong times primitive value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong * primitiveByte, (protectedLong * primitiveByte).value, "protectedLong.times(primitiveByte)")
+        assertEquals(primitiveLong * primitiveShort, (protectedLong * primitiveShort).value, "protectedLong.times(primitiveShort)")
+        assertEquals(primitiveLong * primitiveInt, (protectedLong * primitiveInt).value, "protectedLong.times(primitiveInt)")
+        assertEquals(primitiveLong * primitiveLong, (protectedLong * primitiveLong).value, "protectedLong.times(primitiveLong)")
+        assertEquals(primitiveLong * primitiveFloat, (protectedLong * primitiveFloat).value, "protectedLong.times(primitiveFloat)")
+        assertEquals(primitiveLong * primitiveDouble, (protectedLong * primitiveDouble).value, "protectedLong.times(primitiveDouble)")
+    }
+
+    @Test
+    fun `test the times operator for ProtectedLong times protected value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong * primitiveByte, (protectedLong * protectedByte).value, "protectedLong.times(protectedByte)")
+        assertEquals(primitiveLong * primitiveShort, (protectedLong * protectedShort).value, "protectedLong.times(protectedShort)")
+        assertEquals(primitiveLong * primitiveInt, (protectedLong * protectedInt).value, "protectedLong.times(protectedInt)")
+        assertEquals(primitiveLong * primitiveLong, (protectedLong * protectedLong).value, "protectedLong.times(protectedLong)")
+        assertEquals(primitiveLong * primitiveFloat, (protectedLong * protectedFloat).value, "protectedLong.times(protectedFloat)")
+        assertEquals(primitiveLong * primitiveDouble, (protectedLong * protectedDouble).value, "protectedLong.times(protectedDouble)")
+    }
+
+    @Test
+    fun `test the times operator for primitive value times protected value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong * primitiveByte, primitiveLong * protectedByte, "primitiveLong.times(protectedByte)")
+        assertEquals(primitiveLong * primitiveShort, primitiveLong * protectedShort, "primitiveLong.times(protectedShort)")
+        assertEquals(primitiveLong * primitiveInt, primitiveLong * protectedInt, "primitiveLong.times(protectedInt)")
+        assertEquals(primitiveLong * primitiveLong, primitiveLong * protectedLong, "primitiveLong.times(protectedLong)")
+        assertEquals(primitiveLong * primitiveFloat, primitiveLong * protectedFloat, "primitiveLong.times(protectedFloat)")
+        assertEquals(primitiveLong * primitiveDouble, primitiveLong * protectedDouble, "primitiveLong.times(protectedDouble)")
+    }
+
+    // Div operator
+    @Test
+    fun `test the div operator for ProtectedLong div primitive value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong / primitiveByte, (protectedLong / primitiveByte).value, "protectedLong.div(primitiveByte)")
+        assertEquals(primitiveLong / primitiveShort, (protectedLong / primitiveShort).value, "protectedLong.div(primitiveShort)")
+        assertEquals(primitiveLong / primitiveInt, (protectedLong / primitiveInt).value, "protectedLong.div(primitiveInt)")
+        assertEquals(primitiveLong / primitiveLong, (protectedLong / primitiveLong).value, "protectedLong.div(primitiveLong)")
+        assertEquals(primitiveLong / primitiveFloat, (protectedLong / primitiveFloat).value, "protectedLong.div(primitiveFloat)")
+        assertEquals(primitiveLong / primitiveDouble, (protectedLong / primitiveDouble).value, "protectedLong.div(primitiveDouble)")
+    }
+
+    @Test
+    fun `test the div operator for ProtectedLong div protected value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong / primitiveByte, (protectedLong / protectedByte).value, "protectedLong.div(protectedByte)")
+        assertEquals(primitiveLong / primitiveShort, (protectedLong / protectedShort).value, "protectedLong.div(protectedShort)")
+        assertEquals(primitiveLong / primitiveInt, (protectedLong / protectedInt).value, "protectedLong.div(protectedInt)")
+        assertEquals(primitiveLong / primitiveLong, (protectedLong / protectedLong).value, "protectedLong.div(protectedLong)")
+        assertEquals(primitiveLong / primitiveFloat, (protectedLong / protectedFloat).value, "protectedLong.div(protectedFloat)")
+        assertEquals(primitiveLong / primitiveDouble, (protectedLong / protectedDouble).value, "protectedLong.div(protectedDouble)")
+    }
+
+    @Test
+    fun `test the div operator for primitive value div protected value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong / primitiveByte, primitiveLong / protectedByte, "primitiveLong.div(protectedByte)")
+        assertEquals(primitiveLong / primitiveShort, primitiveLong / protectedShort, "primitiveLong.div(protectedShort)")
+        assertEquals(primitiveLong / primitiveInt, primitiveLong / protectedInt, "primitiveLong.div(protectedInt)")
+        assertEquals(primitiveLong / primitiveLong, primitiveLong / protectedLong, "primitiveLong.div(protectedLong)")
+        assertEquals(primitiveLong / primitiveFloat, primitiveLong / protectedFloat, "primitiveLong.div(protectedFloat)")
+        assertEquals(primitiveLong / primitiveDouble, primitiveLong / protectedDouble, "primitiveLong.div(protectedDouble)")
+    }
+
+    // Rem operator
+    @Test
+    fun `test the rem operator for ProtectedLong rem primitive value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong % primitiveByte, (protectedLong % primitiveByte).value, "protectedLong.rem(primitiveByte)")
+        assertEquals(primitiveLong % primitiveShort, (protectedLong % primitiveShort).value, "protectedLong.rem(primitiveShort)")
+        assertEquals(primitiveLong % primitiveInt, (protectedLong % primitiveInt).value, "protectedLong.rem(primitiveInt)")
+        assertEquals(primitiveLong % primitiveLong, (protectedLong % primitiveLong).value, "protectedLong.rem(primitiveLong)")
+        assertEquals(primitiveLong % primitiveFloat, (protectedLong % primitiveFloat).value, "protectedLong.rem(primitiveFloat)")
+        assertEquals(primitiveLong % primitiveDouble, (protectedLong % primitiveDouble).value, "protectedLong.rem(primitiveDouble)")
+    }
+
+    @Test
+    fun `test the rem operator for ProtectedLong rem protected value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong % primitiveByte, (protectedLong % protectedByte).value, "protectedLong.rem(protectedByte)")
+        assertEquals(primitiveLong % primitiveShort, (protectedLong % protectedShort).value, "protectedLong.rem(protectedShort)")
+        assertEquals(primitiveLong % primitiveInt, (protectedLong % protectedInt).value, "protectedLong.rem(protectedInt)")
+        assertEquals(primitiveLong % primitiveLong, (protectedLong % protectedLong).value, "protectedLong.rem(protectedLong)")
+        assertEquals(primitiveLong % primitiveFloat, (protectedLong % protectedFloat).value, "protectedLong.rem(protectedFloat)")
+        assertEquals(primitiveLong % primitiveDouble, (protectedLong % protectedDouble).value, "protectedLong.rem(protectedDouble)")
+    }
+
+    @Test
+    fun `test the rem operator for primitive value rem protected value and compares the result with the same operation with native Long type`() {
+        assertEquals(primitiveLong % primitiveByte, primitiveLong % protectedByte, "primitiveLong.rem(protectedByte)")
+        assertEquals(primitiveLong % primitiveShort, primitiveLong % protectedShort, "primitiveLong.rem(protectedShort)")
+        assertEquals(primitiveLong % primitiveInt, primitiveLong % protectedInt, "primitiveLong.rem(protectedInt)")
+        assertEquals(primitiveLong % primitiveLong, primitiveLong % protectedLong, "primitiveLong.rem(protectedLong)")
+        assertEquals(primitiveLong % primitiveFloat, primitiveLong % protectedFloat, "primitiveLong.rem(protectedFloat)")
+        assertEquals(primitiveLong % primitiveDouble, primitiveLong % protectedDouble, "primitiveLong.rem(protectedDouble)")
+    }
+}
