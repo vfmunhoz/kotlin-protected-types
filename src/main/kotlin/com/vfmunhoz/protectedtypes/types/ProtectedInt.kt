8885ee39d1d08defea48914683974832b86c492c
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2021-06-27 22:29:33 -0300

    feat(protected-int): add more use cases for int operators and unit tests

--- src/main/kotlin/com/vfmunhoz/protectedtypes/types/ProtectedInt.kt
+++ src/main/kotlin/com/vfmunhoz/protectedtypes/types/ProtectedInt.kt
@@ -17,6 +17,13 @@ value class ProtectedInt(val value: Int) : Comparable<ProtectedInt> {
     operator fun plus(other: Float): ProtectedFloat = ProtectedFloat(value + other)
     operator fun plus(other: Double): ProtectedDouble = ProtectedDouble(value + other)
 
+    operator fun plus(other: ProtectedByte): ProtectedInt = this.plus(other.value)
+    operator fun plus(other: ProtectedShort): ProtectedInt = this.plus(other.value)
+    operator fun plus(other: ProtectedInt): ProtectedInt = this.plus(other.value)
+    operator fun plus(other: ProtectedLong): ProtectedLong = this.plus(other.value)
+    operator fun plus(other: ProtectedFloat): ProtectedFloat = this.plus(other.value)
+    operator fun plus(other: ProtectedDouble): ProtectedDouble = this.plus(other.value)
+
     // Minus
     operator fun minus(other: Byte): ProtectedInt = ProtectedInt(value - other)
     operator fun minus(other: Short): ProtectedInt = ProtectedInt(value - other)
@@ -24,6 +31,13 @@ value class ProtectedInt(val value: Int) : Comparable<ProtectedInt> {
     operator fun minus(other: Long): ProtectedLong = ProtectedLong(value - other)
     operator fun minus(other: Float): ProtectedFloat = ProtectedFloat(value - other)
     operator fun minus(other: Double): ProtectedDouble = ProtectedDouble(value - other)
+
+    operator fun minus(other: ProtectedByte): ProtectedInt = this.minus(other.value)
+    operator fun minus(other: ProtectedShort): ProtectedInt = this.minus(other.value)
+    operator fun minus(other: ProtectedInt): ProtectedInt = this.minus(other.value)
+    operator fun minus(other: ProtectedLong): ProtectedLong = this.minus(other.value)
+    operator fun minus(other: ProtectedFloat): ProtectedFloat = this.minus(other.value)
+    operator fun minus(other: ProtectedDouble): ProtectedDouble = this.minus(other.value)
 
     // Times
     operator fun times(other: Byte): ProtectedInt = ProtectedInt(value * other)
@@ -33,6 +47,13 @@ value class ProtectedInt(val value: Int) : Comparable<ProtectedInt> {
     operator fun times(other: Float): ProtectedFloat = ProtectedFloat(value * other)
     operator fun times(other: Double): ProtectedDouble = ProtectedDouble(value * other)
 
+    operator fun times(other: ProtectedByte): ProtectedInt = this.times(other.value)
+    operator fun times(other: ProtectedShort): ProtectedInt = this.times(other.value)
+    operator fun times(other: ProtectedInt): ProtectedInt = this.times(other.value)
+    operator fun times(other: ProtectedLong): ProtectedLong = this.times(other.value)
+    operator fun times(other: ProtectedFloat): ProtectedFloat = this.times(other.value)
+    operator fun times(other: ProtectedDouble): ProtectedDouble = this.times(other.value)
+
     // Div
     operator fun div(other: Byte): ProtectedInt = ProtectedInt(value / other)
     operator fun div(other: Short): ProtectedInt = ProtectedInt(value / other)
@@ -40,6 +61,13 @@ value class ProtectedInt(val value: Int) : Comparable<ProtectedInt> {
     operator fun div(other: Long): ProtectedLong = ProtectedLong(value / other)
     operator fun div(other: Float): ProtectedFloat = ProtectedFloat(value / other)
     operator fun div(other: Double): ProtectedDouble = ProtectedDouble(value / other)
+
+    operator fun div(other: ProtectedByte): ProtectedInt = this.div(other.value)
+    operator fun div(other: ProtectedShort): ProtectedInt = this.div(other.value)
+    operator fun div(other: ProtectedInt): ProtectedInt = this.div(other.value)
+    operator fun div(other: ProtectedLong): ProtectedLong = this.div(other.value)
+    operator fun div(other: ProtectedFloat): ProtectedFloat = this.div(other.value)
+    operator fun div(other: ProtectedDouble): ProtectedDouble = this.div(other.value)
 
     // Rem
     operator fun rem(other: Byte): ProtectedInt = ProtectedInt(value % other)
@@ -49,8 +77,55 @@ value class ProtectedInt(val value: Int) : Comparable<ProtectedInt> {
     operator fun rem(other: Float): ProtectedFloat = ProtectedFloat(value % other)
     operator fun rem(other: Double): ProtectedDouble = ProtectedDouble(value % other)
 
+    operator fun rem(other: ProtectedByte): ProtectedInt = this.rem(other.value)
+    operator fun rem(other: ProtectedShort): ProtectedInt = this.rem(other.value)
+    operator fun rem(other: ProtectedInt): ProtectedInt = this.rem(other.value)
+    operator fun rem(other: ProtectedLong): ProtectedLong = this.rem(other.value)
+    operator fun rem(other: ProtectedFloat): ProtectedFloat = this.rem(other.value)
+    operator fun rem(other: ProtectedDouble): ProtectedDouble = this.rem(other.value)
+
     operator fun compareTo(other: Int): Int = value.compareTo(other)
     override operator fun compareTo(other: ProtectedInt): Int = compareTo(other.value)
 
     override fun toString(): String = "###$value###"
 }
+
+// Primitive + protected
+operator fun Int.plus(protectedValue: ProtectedByte): Int = this + protectedValue.value
+operator fun Int.plus(protectedValue: ProtectedShort): Int = this + protectedValue.value
+operator fun Int.plus(protectedValue: ProtectedInt): Int = this + protectedValue.value
+operator fun Int.plus(protectedValue: ProtectedLong): Long = this + protectedValue.value
+operator fun Int.plus(protectedValue: ProtectedFloat): Float = this + protectedValue.value
+operator fun Int.plus(protectedValue: ProtectedDouble): Double = this + protectedValue.value
+
+// Primitive + protected
+operator fun Int.minus(protectedValue: ProtectedByte): Int = this - protectedValue.value
+operator fun Int.minus(protectedValue: ProtectedShort): Int = this - protectedValue.value
+operator fun Int.minus(protectedValue: ProtectedInt): Int = this - protectedValue.value
+operator fun Int.minus(protectedValue: ProtectedLong): Long = this - protectedValue.value
+operator fun Int.minus(protectedValue: ProtectedFloat): Float = this - protectedValue.value
+operator fun Int.minus(protectedValue: ProtectedDouble): Double = this - protectedValue.value
+
+// Primitive + protected
+operator fun Int.times(protectedValue: ProtectedByte): Int = this * protectedValue.value
+operator fun Int.times(protectedValue: ProtectedShort): Int = this * protectedValue.value
+operator fun Int.times(protectedValue: ProtectedInt): Int = this * protectedValue.value
+operator fun Int.times(protectedValue: ProtectedLong): Long = this * protectedValue.value
+operator fun Int.times(protectedValue: ProtectedFloat): Float = this * protectedValue.value
+operator fun Int.times(protectedValue: ProtectedDouble): Double = this * protectedValue.value
+
+// Primitive + protected
+operator fun Int.div(protectedValue: ProtectedByte): Int = this / protectedValue.value
+operator fun Int.div(protectedValue: ProtectedShort): Int = this / protectedValue.value
+operator fun Int.div(protectedValue: ProtectedInt): Int = this / protectedValue.value
+operator fun Int.div(protectedValue: ProtectedLong): Long = this / protectedValue.value
+operator fun Int.div(protectedValue: ProtectedFloat): Float = this / protectedValue.value
+operator fun Int.div(protectedValue: ProtectedDouble): Double = this / protectedValue.value
+
+// Primitive + protected
+operator fun Int.rem(protectedValue: ProtectedByte): Int = this % protectedValue.value
+operator fun Int.rem(protectedValue: ProtectedShort): Int = this % protectedValue.value
+operator fun Int.rem(protectedValue: ProtectedInt): Int = this % protectedValue.value
+operator fun Int.rem(protectedValue: ProtectedLong): Long = this % protectedValue.value
+operator fun Int.rem(protectedValue: ProtectedFloat): Float = this % protectedValue.value
+operator fun Int.rem(protectedValue: ProtectedDouble): Double = this % protectedValue.value
---
+++ src/test/kotlin/com/vfmunhoz/protectedtypes/types/ProtectedIntTest.kt
@@ -0,0 +1,251 @@
+package com.vfmunhoz.protectedtypes.types
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class ProtectedIntTest {
+
+    private val primitiveByte: Byte = 10
+    private val primitiveShort: Short = 12
+    private val primitiveInt: Int = 14
+    private val primitiveLong: Long = 16
+    private val primitiveFloat: Float = 18.2f
+    private val primitiveDouble: Double = 20.8
+
+    private val protectedByte = ProtectedByte(primitiveByte)
+    private val protectedShort = ProtectedShort(primitiveShort)
+    private val protectedInt = ProtectedInt(primitiveInt)
+    private val protectedLong = ProtectedLong(primitiveLong)
+    private val protectedFloat = ProtectedFloat(primitiveFloat)
+    private val protectedDouble = ProtectedDouble(primitiveDouble)
+
+    private val maxProtected = ProtectedInt(Int.MAX_VALUE)
+    private val minProtected = ProtectedInt(Int.MIN_VALUE)
+
+    // Unary operators
+    @Test
+    fun `test the unary plus operator compared to Int type`() {
+        assertEquals(+primitiveInt, (+protectedInt).value)
+        assertEquals(primitiveInt.unaryPlus(), protectedInt.unaryPlus().value)
+
+        assertEquals(+Int.MAX_VALUE, (+maxProtected).value)
+        assertEquals(+Int.MIN_VALUE, (+minProtected).value)
+
+        assertEquals(Int.MAX_VALUE.unaryPlus(), (maxProtected.unaryPlus()).value)
+        assertEquals(Int.MIN_VALUE.unaryPlus(), (minProtected.unaryPlus()).value)
+    }
+
+    @Test
+    fun `test the unary minus operator compared to Int type`() {
+        assertEquals(-primitiveInt, (-protectedInt).value)
+        assertEquals(primitiveInt.unaryMinus(), protectedInt.unaryMinus().value)
+
+        assertEquals(-Int.MAX_VALUE, (-maxProtected).value)
+        assertEquals(-Int.MIN_VALUE, (-minProtected).value)
+
+        assertEquals(Int.MAX_VALUE.unaryMinus(), (maxProtected.unaryMinus()).value)
+        assertEquals(Int.MIN_VALUE.unaryMinus(), (minProtected.unaryMinus()).value)
+    }
+
+    // Increment and decrement operators
+    @Test
+    fun `test the increment operator compared to Int type`() {
+        var mutablePrimitive = primitiveInt
+        var mutableProtected = protectedInt
+
+        ++mutablePrimitive
+        ++mutableProtected
+
+        assertEquals(mutablePrimitive, mutableProtected.value)
+
+        mutablePrimitive++
+        mutableProtected++
+
+        assertEquals(mutablePrimitive, mutableProtected.value)
+
+        assertEquals(mutablePrimitive.inc(), mutableProtected.inc().value)
+
+        var mutableMaxPrimitive = Int.MAX_VALUE
+        var mutableMaxProtected = maxProtected
+
+        assertEquals(++mutableMaxPrimitive, (++mutableMaxProtected).value)
+    }
+
+    @Test
+    fun `test the decrement operator compared to Int type`() {
+        var mutablePrimitive = primitiveInt
+        var mutableProtected = protectedInt
+
+        --mutablePrimitive
+        --mutableProtected
+
+        assertEquals(mutablePrimitive, mutableProtected.value)
+
+        mutablePrimitive--
+        mutableProtected--
+
+        assertEquals(mutablePrimitive, mutableProtected.value)
+
+        assertEquals(mutablePrimitive.dec(), mutableProtected.dec().value)
+
+        var mutableMaxPrimitiveDouble = Int.MIN_VALUE
+        var mutableMaxProtectedDouble = minProtected
+
+        assertEquals(--mutableMaxPrimitiveDouble, (--mutableMaxProtectedDouble).value)
+    }
+
+    // Plus operator
+    @Test
+    fun `test the sum operator for ProtectedInt + primitive value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt + primitiveByte, (protectedInt + primitiveByte).value, "protectedInt.sum(primitiveByte)")
+        assertEquals(primitiveInt + primitiveShort, (protectedInt + primitiveShort).value, "protectedInt.sum(primitiveShort)")
+        assertEquals(primitiveInt + primitiveInt, (protectedInt + primitiveInt).value, "protectedInt.sum(primitiveInt)")
+        assertEquals(primitiveInt + primitiveLong, (protectedInt + primitiveLong).value, "protectedInt.sum(primitiveLong)")
+        assertEquals(primitiveInt + primitiveFloat, (protectedInt + primitiveFloat).value, "protectedInt.sum(primitiveFloat)")
+        assertEquals(primitiveInt + primitiveDouble, (protectedInt + primitiveDouble).value, "protectedInt.sum(primitiveDouble)")
+    }
+
+    @Test
+    fun `test the sum operator for ProtectedInt + protected value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt + primitiveByte, (protectedInt + protectedByte).value, "protectedInt.sum(protectedByte)")
+        assertEquals(primitiveInt + primitiveShort, (protectedInt + protectedShort).value, "protectedInt.sum(protectedShort)")
+        assertEquals(primitiveInt + primitiveInt, (protectedInt + protectedInt).value, "protectedInt.sum(protectedInt)")
+        assertEquals(primitiveInt + primitiveLong, (protectedInt + protectedLong).value, "protectedInt.sum(protectedLong)")
+        assertEquals(primitiveInt + primitiveFloat, (protectedInt + protectedFloat).value, "protectedInt.sum(protectedFloat)")
+        assertEquals(primitiveInt + primitiveDouble, (protectedInt + protectedDouble).value, "protectedInt.sum(protectedDouble)")
+    }
+
+    @Test
+    fun `test the sum operator for primitive value + protected value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt + primitiveByte, primitiveInt + protectedByte, "primitiveInt.sum(protectedByte)")
+        assertEquals(primitiveInt + primitiveShort, primitiveInt + protectedShort, "primitiveInt.sum(protectedShort)")
+        assertEquals(primitiveInt + primitiveInt, primitiveInt + protectedInt, "primitiveInt.sum(protectedInt)")
+        assertEquals(primitiveInt + primitiveLong, primitiveInt + protectedLong, "primitiveInt.sum(protectedLong)")
+        assertEquals(primitiveInt + primitiveFloat, primitiveInt + protectedFloat, "primitiveInt.sum(protectedFloat)")
+        assertEquals(primitiveInt + primitiveDouble, primitiveInt + protectedDouble, "primitiveInt.sum(protectedDouble)")
+    }
+
+    // Minus operator
+    @Test
+    fun `test the minus operator for ProtectedInt - primitive value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt - primitiveByte, (protectedInt - primitiveByte).value, "protectedInt.minus(primitiveByte)")
+        assertEquals(primitiveInt - primitiveShort, (protectedInt - primitiveShort).value, "protectedInt.minus(primitiveShort)")
+        assertEquals(primitiveInt - primitiveInt, (protectedInt - primitiveInt).value, "protectedInt.minus(primitiveInt)")
+        assertEquals(primitiveInt - primitiveLong, (protectedInt - primitiveLong).value, "protectedInt.minus(primitiveLong)")
+        assertEquals(primitiveInt - primitiveFloat, (protectedInt - primitiveFloat).value, "protectedInt.minus(primitiveFloat)")
+        assertEquals(primitiveInt - primitiveDouble, (protectedInt - primitiveDouble).value, "protectedInt.minus(primitiveDouble)")
+    }
+
+    @Test
+    fun `test the minus operator for ProtectedInt - protected value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt - primitiveByte, (protectedInt - protectedByte).value, "protectedInt.minus(protectedByte)")
+        assertEquals(primitiveInt - primitiveShort, (protectedInt - protectedShort).value, "protectedInt.minus(protectedShort)")
+        assertEquals(primitiveInt - primitiveInt, (protectedInt - protectedInt).value, "protectedInt.minus(protectedInt)")
+        assertEquals(primitiveInt - primitiveLong, (protectedInt - protectedLong).value, "protectedInt.minus(protectedLong)")
+        assertEquals(primitiveInt - primitiveFloat, (protectedInt - protectedFloat).value, "protectedInt.minus(protectedFloat)")
+        assertEquals(primitiveInt - primitiveDouble, (protectedInt - protectedDouble).value, "protectedInt.minus(protectedDouble)")
+    }
+
+    @Test
+    fun `test the minus operator for primitive value - protected value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt - primitiveByte, primitiveInt - protectedByte, "primitiveInt.minus(protectedByte)")
+        assertEquals(primitiveInt - primitiveShort, primitiveInt - protectedShort, "primitiveInt.minus(protectedShort)")
+        assertEquals(primitiveInt - primitiveInt, primitiveInt - protectedInt, "primitiveInt.minus(protectedInt)")
+        assertEquals(primitiveInt - primitiveLong, primitiveInt - protectedLong, "primitiveInt.minus(protectedLong)")
+        assertEquals(primitiveInt - primitiveFloat, primitiveInt - protectedFloat, "primitiveInt.minus(protectedFloat)")
+        assertEquals(primitiveInt - primitiveDouble, primitiveInt - protectedDouble, "primitiveInt.minus(protectedDouble)")
+    }
+
+    // Times operator
+    @Test
+    fun `test the times operator for ProtectedInt times primitive value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt * primitiveByte, (protectedInt * primitiveByte).value, "protectedInt.times(primitiveByte)")
+        assertEquals(primitiveInt * primitiveShort, (protectedInt * primitiveShort).value, "protectedInt.times(primitiveShort)")
+        assertEquals(primitiveInt * primitiveInt, (protectedInt * primitiveInt).value, "protectedInt.times(primitiveInt)")
+        assertEquals(primitiveInt * primitiveLong, (protectedInt * primitiveLong).value, "protectedInt.times(primitiveLong)")
+        assertEquals(primitiveInt * primitiveFloat, (protectedInt * primitiveFloat).value, "protectedInt.times(primitiveFloat)")
+        assertEquals(primitiveInt * primitiveDouble, (protectedInt * primitiveDouble).value, "protectedInt.times(primitiveDouble)")
+    }
+
+    @Test
+    fun `test the times operator for ProtectedInt times protected value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt * primitiveByte, (protectedInt * protectedByte).value, "protectedInt.times(protectedByte)")
+        assertEquals(primitiveInt * primitiveShort, (protectedInt * protectedShort).value, "protectedInt.times(protectedShort)")
+        assertEquals(primitiveInt * primitiveInt, (protectedInt * protectedInt).value, "protectedInt.times(protectedInt)")
+        assertEquals(primitiveInt * primitiveLong, (protectedInt * protectedLong).value, "protectedInt.times(protectedLong)")
+        assertEquals(primitiveInt * primitiveFloat, (protectedInt * protectedFloat).value, "protectedInt.times(protectedFloat)")
+        assertEquals(primitiveInt * primitiveDouble, (protectedInt * protectedDouble).value, "protectedInt.times(protectedDouble)")
+    }
+
+    @Test
+    fun `test the times operator for primitive value times protected value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt * primitiveByte, primitiveInt * protectedByte, "primitiveInt.times(protectedByte)")
+        assertEquals(primitiveInt * primitiveShort, primitiveInt * protectedShort, "primitiveInt.times(protectedShort)")
+        assertEquals(primitiveInt * primitiveInt, primitiveInt * protectedInt, "primitiveInt.times(protectedInt)")
+        assertEquals(primitiveInt * primitiveLong, primitiveInt * protectedLong, "primitiveInt.times(protectedLong)")
+        assertEquals(primitiveInt * primitiveFloat, primitiveInt * protectedFloat, "primitiveInt.times(protectedFloat)")
+        assertEquals(primitiveInt * primitiveDouble, primitiveInt * protectedDouble, "primitiveInt.times(protectedDouble)")
+    }
+
+    // Div operator
+    @Test
+    fun `test the div operator for ProtectedInt div primitive value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt / primitiveByte, (protectedInt / primitiveByte).value, "protectedInt.div(primitiveByte)")
+        assertEquals(primitiveInt / primitiveShort, (protectedInt / primitiveShort).value, "protectedInt.div(primitiveShort)")
+        assertEquals(primitiveInt / primitiveInt, (protectedInt / primitiveInt).value, "protectedInt.div(primitiveInt)")
+        assertEquals(primitiveInt / primitiveLong, (protectedInt / primitiveLong).value, "protectedInt.div(primitiveLong)")
+        assertEquals(primitiveInt / primitiveFloat, (protectedInt / primitiveFloat).value, "protectedInt.div(primitiveFloat)")
+        assertEquals(primitiveInt / primitiveDouble, (protectedInt / primitiveDouble).value, "protectedInt.div(primitiveDouble)")
+    }
+
+    @Test
+    fun `test the div operator for ProtectedInt div protected value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt / primitiveByte, (protectedInt / protectedByte).value, "protectedInt.div(protectedByte)")
+        assertEquals(primitiveInt / primitiveShort, (protectedInt / protectedShort).value, "protectedInt.div(protectedShort)")
+        assertEquals(primitiveInt / primitiveInt, (protectedInt / protectedInt).value, "protectedInt.div(protectedInt)")
+        assertEquals(primitiveInt / primitiveLong, (protectedInt / protectedLong).value, "protectedInt.div(protectedLong)")
+        assertEquals(primitiveInt / primitiveFloat, (protectedInt / protectedFloat).value, "protectedInt.div(protectedFloat)")
+        assertEquals(primitiveInt / primitiveDouble, (protectedInt / protectedDouble).value, "protectedInt.div(protectedDouble)")
+    }
+
+    @Test
+    fun `test the div operator for primitive value div protected value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt / primitiveByte, primitiveInt / protectedByte, "primitiveInt.div(protectedByte)")
+        assertEquals(primitiveInt / primitiveShort, primitiveInt / protectedShort, "primitiveInt.div(protectedShort)")
+        assertEquals(primitiveInt / primitiveInt, primitiveInt / protectedInt, "primitiveInt.div(protectedInt)")
+        assertEquals(primitiveInt / primitiveLong, primitiveInt / protectedLong, "primitiveInt.div(protectedLong)")
+        assertEquals(primitiveInt / primitiveFloat, primitiveInt / protectedFloat, "primitiveInt.div(protectedFloat)")
+        assertEquals(primitiveInt / primitiveDouble, primitiveInt / protectedDouble, "primitiveInt.div(protectedDouble)")
+    }
+
+    // Rem operator
+    @Test
+    fun `test the rem operator for ProtectedInt rem primitive value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt % primitiveByte, (protectedInt % primitiveByte).value, "protectedInt.rem(primitiveByte)")
+        assertEquals(primitiveInt % primitiveShort, (protectedInt % primitiveShort).value, "protectedInt.rem(primitiveShort)")
+        assertEquals(primitiveInt % primitiveInt, (protectedInt % primitiveInt).value, "protectedInt.rem(primitiveInt)")
+        assertEquals(primitiveInt % primitiveLong, (protectedInt % primitiveLong).value, "protectedInt.rem(primitiveLong)")
+        assertEquals(primitiveInt % primitiveFloat, (protectedInt % primitiveFloat).value, "protectedInt.rem(primitiveFloat)")
+        assertEquals(primitiveInt % primitiveDouble, (protectedInt % primitiveDouble).value, "protectedInt.rem(primitiveDouble)")
+    }
+
+    @Test
+    fun `test the rem operator for ProtectedInt rem protected value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt % primitiveByte, (protectedInt % protectedByte).value, "protectedInt.rem(protectedByte)")
+        assertEquals(primitiveInt % primitiveShort, (protectedInt % protectedShort).value, "protectedInt.rem(protectedShort)")
+        assertEquals(primitiveInt % primitiveInt, (protectedInt % protectedInt).value, "protectedInt.rem(protectedInt)")
+        assertEquals(primitiveInt % primitiveLong, (protectedInt % protectedLong).value, "protectedInt.rem(protectedLong)")
+        assertEquals(primitiveInt % primitiveFloat, (protectedInt % protectedFloat).value, "protectedInt.rem(protectedFloat)")
+        assertEquals(primitiveInt % primitiveDouble, (protectedInt % protectedDouble).value, "protectedInt.rem(protectedDouble)")
+    }
+
+    @Test
+    fun `test the rem operator for primitive value rem protected value and compares the result with the same operation with native Int type`() {
+        assertEquals(primitiveInt % primitiveByte, primitiveInt % protectedByte, "primitiveInt.rem(protectedByte)")
+        assertEquals(primitiveInt % primitiveShort, primitiveInt % protectedShort, "primitiveInt.rem(protectedShort)")
+        assertEquals(primitiveInt % primitiveInt, primitiveInt % protectedInt, "primitiveInt.rem(protectedInt)")
+        assertEquals(primitiveInt % primitiveLong, primitiveInt % protectedLong, "primitiveInt.rem(protectedLong)")
+        assertEquals(primitiveInt % primitiveFloat, primitiveInt % protectedFloat, "primitiveInt.rem(protectedFloat)")
+        assertEquals(primitiveInt % primitiveDouble, primitiveInt % protectedDouble, "primitiveInt.rem(protectedDouble)")
+    }
+}
