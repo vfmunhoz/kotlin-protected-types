9bf283dc212859ea32f878587c084e321929d192
Vinicius Montes Munhoz <viniciusfmunhoz@gmail.com>
2021-08-27 14:17:00 -0300

    ci(configs): remove legacy maven central url

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ publishing {
         maven {
             name = "oss"
 
-            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
 
             url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
 
